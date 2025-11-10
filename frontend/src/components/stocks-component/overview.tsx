af2579415bad00f1a078b2a33788a24b51b7f6eb
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-11-10 07:14:43 -0500

    overview done

--- frontend/src/components/stocks-component/overview.tsx
+++ frontend/src/components/stocks-component/overview.tsx
@@ -3,6 +3,15 @@ import { OverviewProps } from '../../types/items';
 
 function Overview({ data }: OverviewProps) {
     console.log("overview", data)
+
+    const totalProducts = data.length;
+
+    const activeItems = data.filter(item => item.status?.toLowerCase() === "active").length;
+
+    const lowStock = data.filter(item => item.quantity <= 10 && item.quantity > 0).length;
+
+    const outOfStock = data.filter(item => item.quantity === 0).length;
+
     return (
         <div className='overview'>
             <div className='card'>
@@ -10,7 +19,18 @@ function Overview({ data }: OverviewProps) {
                     Total Products
                 </div>
                 <div className='cardContent'>
-                    X
+                    {totalProducts}
+                </div>
+                <div className='subtitle'>
+                    Total inventory items
+                </div>
+            </div>
+            <div className='card'>
+                <div className='subtitle'>
+                    Active Products
+                </div>
+                <div className='cardContent'>
+                    {activeItems}
                 </div>
                 <div className='subtitle'>
                     Active inventory items
@@ -21,7 +41,7 @@ function Overview({ data }: OverviewProps) {
                     Low Stock
                 </div>
                 <div className='cardContent'>
-                    X
+                    {lowStock}
                 </div>
                 <div className='subtitle'>
                     Items with ≤ 10 units
@@ -31,7 +51,7 @@ function Overview({ data }: OverviewProps) {
                     Out of Stock
                 </div>
                 <div className='cardContent'>
-                    X
+                    {outOfStock}
                 </div>
                 <div className='subtitle'>
                     Item needs restocking
